2f0ff12777dc9419cb6460f9cce39f5e3746863c
ArmenOzcelik <112550415+ArmenOzcelik@users.noreply.github.com>
2023-11-27 15:05:08 +0100

    tech(585): doing async calls for JURITJ API scenarios

--- src/juritj-api/0-test-connection.js
+++ src/juritj-api/0-test-connection.js
@@ -1,35 +1,13 @@
-const { mandatoryDecisionMetadata } = require('./utils')
+const { autocannonConf } = require('./utils')
 
 /*
  * This first scenario aims to successfully call the endpoint POST /decisions on JURITJ API.
  */
 const autocannon = require('autocannon')
 
-console.log({ ca: process.env.CA_CERT })
-console.log({ cert: process.env.CLIENT_CERT })
-console.log({ key: process.env.CLIENT_PRIVATE_KEY })
-console.log({ passphrase: process.env.CLIENT_PRIVATE_KEY_PASSPHRASE })
-
 autocannon(
   {
-    title: 'POST /decisions on JURITJ API',
-    url: `${process.env.JURITJ_API_URL}/v1/decisions`,
-    method: 'POST',
-    form: {
-      decisionIntegre: {
-        type: 'file',
-        path: 'src/juritj-api/wordperfect-example-file.wpd'
-      },
-      metadonnees: {
-        type: 'text',
-        value: JSON.stringify(mandatoryDecisionMetadata)
-      }
-    },
-    tlsOptions: {
-      ca: process.env.CA_CERT,
-      cert: process.env.CLIENT_CERT,
-      key: process.env.CLIENT_PRIVATE_KEY
-    },
+    ...autocannonConf,
     amount: 50
   },
   console.log

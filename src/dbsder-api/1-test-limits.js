068db615f086520d7cfd900691e27a1783f91e82
Benjamin PILIA <benjamin.pilia@octo.com>
2024-01-16 14:39:35 +0100

    test en direct 100 decisions

--- src/dbsder-api/1-test-limits.js
+++ src/dbsder-api/1-test-limits.js
@@ -30,36 +30,9 @@ async function runLoad(amount) {
 
 async function runScenario() {
   console.log('========> Load 1 <========')
-  await runLoad(100).then(async () => {
-    console.log('========> Load 2 <========')
-    await runLoad(500).then(async () => {
-      console.log('========> Load 3 <========')
-      await runLoad(1000).then(async () => {
-        console.log('========> Load 4 <========')
-        await runLoad(5000).then(async () => {
-          console.log('========> Load 5 <========')
-          await runLoad(10000).then(async () => {
-            console.log('========> Load 6 <========')
-            await runLoad(20000).then(async () => {
-              console.log('========> Load 7 <========')
-              await runLoad(40000).then(async () => {
-                console.log('========> Load 8 <========')
-                await runLoad(60000).then(async () => {
-                  console.log('========> Load 9 <========')
-                  await runLoad(80000).then(async () => {
-                    console.log('========> Load 10 <========')
-                    await runLoad(100000).then(async (createdDecisionId) => {
-                      console.log('========> Deleting decision <========')
-                      await deleteDecision(createdDecisionId)
-                    })
-                  })
-                })
-              })
-            })
-          })
-        })
-      })
-    })
+  await runLoad(100).then(async (createdDecisionId) => {
+    console.log('========> Deleting decision <========')
+
   })
 }
 

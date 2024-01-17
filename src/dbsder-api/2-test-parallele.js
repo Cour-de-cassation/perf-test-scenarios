401c5959d2355a130619c4d9a0623a3903de9337
Ritchie <ritchie.velmir@octo.com>
2024-01-17 16:23:19 +0100

    tech: feedbacks on PR

--- src/dbsder-api/2-test-parallele.js
+++ src/dbsder-api/2-test-parallele.js
@@ -1,12 +1,12 @@
 const autocannon = require('autocannon')
 const { decision, deleteDecision } = require('./utils')
 
-async function createDecision(workers, amount) {
-  var createdDecisionId = null
+async function createDecision(connections, amount) {
+  let createdDecisionId = null
   const instance = await autocannon({
     url: `${process.env.DBSDER_API_URL}`,
     amount: amount,
-    connections: workers,
+    connections: connections,
     requests: [
       {
         title: 'PUT /api/v1/decisions',
@@ -32,20 +32,11 @@ async function createDecision(workers, amount) {
 
 async function runTestConnection() {
   console.log('========> Test Scenario 2 Parallel connection <========')
-  const connectionsLevels = [1, 2, 3, 4, 5]
-  const requestAmounts = [100, 200, 300, 400, 500]
-
-  // Make the first call outside the loop to get the ID
   const createdDecisionId = await createDecision(1, 1)
+  await createDecision(50, 200000)
 
-  for (const connections of connectionsLevels) {
-    for (const amount of requestAmounts) {
-      console.log(`========> Test Scenario 2 for ${connections} connections and an amount of ${amount} <========`)
-      await createDecision(connections, amount)
-    }
-  }
   console.log('========> Cleaning test decision <========')
-  deleteDecision(createdDecisionId)
+  await deleteDecision(createdDecisionId)
 }
 
 runTestConnection()
